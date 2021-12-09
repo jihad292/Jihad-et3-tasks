1b4b88c9b5408282190ed0bb590e42403b3352b7
jihad292 <jihad_chamoun1221@outlook.com>
2021-12-09 18:54:31 +0200

    added icons to home Page

--- screens/home.tsx
+++ screens/home.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { View, Text, StyleSheet } from 'react-native';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
 const HomeScreen = () => {
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor:'rgb(204, 204, 255)' }}>
-         
+          <FontAwesome5 name={'robot'} size={80} color={'olivedrab'} />
           <Text style={styles.text}>Welcome To your Prs Application </Text>
+          <FontAwesome5 name={'exclamation-triangle'} size={80} color={'olivedrab'} />
         </View>
       );
     }
